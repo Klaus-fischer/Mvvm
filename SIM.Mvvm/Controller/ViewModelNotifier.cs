3e11b83ef848e11321134c0caec65ba14b5edd72
Klaus-fischer <corb-bartloff@web.de>
2021-07-22 08:33:17 +0200

    Create ViewModelNotifier.cs

---
+++ SIM.Mvvm/Controller/ViewModelNotifier.cs
@@ -0,0 +1,35 @@
+namespace SIM.Mvvm;
+{
+class ViewModelNotifier
+{
+    readonly IViewModel viewModel;
+
+    readonly Collection<string> properties = new();
+
+    public ViewModelNotifier(IViewModel viewModel)
+    {
+        this.viewModel = viewModel;
+    }
+
+    public void AddProperty(string property)
+    {
+        if (!this.properties.Contains(property))
+        {
+            this.properties.Add(property);
+        }
+    }
+
+    public void RemoveProperty(string property)
+    {
+        this.propertys.Remove(property);
+    }
+
+    public void InvokePropertyChanged()
+    {
+        foreach (var property in this.properties)
+        {
+            this.viewModel.InvokeOnProprtyChanged(this.viewModel, property);
+        }
+    }
+}
+}
